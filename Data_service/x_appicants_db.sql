6ea157cb1a0389ad0c6857da7f1bfbda9f994b95
oyama <oyama@projectcodex.co>
2015-12-17 20:11:20 +0200

    created tables for recently created forms the ones that are on a good state

--- Data_service/x_appicants_db.sql
+++ Data_service/x_appicants_db.sql
@@ -2,27 +2,29 @@ CREATE DATABASE codeX_applicants_db;
 
 USE codeX_applicants_db;
  
-CREATE TABLE reference_td (
+CREATE TABLE IF NOT EXISTS `reference_td` (
   `reference_id` int(200) NOT NULL AUTO_INCREMENT,
-  `name` text NOT NULL,
+  `name` text(20) NOT NULL,
   `surname` text(20) NOT NULL,
-  `email` varchar(20) NOT NULL,
+  `email` varchar(50) NOT NULL,
   `cell_no` int(10) NOT NULL,
   PRIMARY KEY (`reference_id`)
 );
 
 -- Table structure for table `codecademy_mailAdrresses`
 
-CREATE TABLE codecademy_mailAdrresses (
+CREATE TABLE IF NOT EXISTS `codecademy_mailAdrresses` (
   `mail_id` int(200) NOT NULL AUTO_INCREMENT,
-  `email` varchar(20) NOT NULL,
+  `email` VARCHAR(30),
    PRIMARY KEY (`mail_id`)
 );
 
 -- Table structure for table `applicants_td`
---
-CREATE TABLE applicants_td (
-  `applicant_id` int NOT NULL AUTO_INCREMENT,
+
+CREATE TABLE IF NOT EXISTS `applicants_td` (
+  `applicant_id` int(200) NOT NULL AUTO_INCREMENT,
+  `coded_before` text(20) NOT NULL,
+  `experiance` varchar(400) NOT NULL,
   `name` text(20) NOT NULL,                                
   `surname` text(20) NOT NULL,
   `email` varchar(20) NOT NULL,
@@ -30,26 +32,18 @@ CREATE TABLE applicants_td (
   `dob` Date NOT NULL,
   `city` text(20) NOT NULL,
   `educational_info` varchar(400) NOT NULL,
+  `mail_id` int(200) DEFAULT NULL,
   `reference_id` int(200) DEFAULT NULL, 
   `applicant_ref_link` varchar(20) NOT NULL,
   PRIMARY KEY (`applicant_id`),
-  KEY `mail_id` (`mail_id`)
-  KEY `reference_id` (`reference_id`)
+  FOREIGN KEY (`mail_id`) REFERENCES codecademy_mailAdrresses(`mail_id`),
+  FOREIGN KEY (`reference_id`) REFERENCES reference_td(`reference_id`)
 );
 
+-- Dumping data for table --
 
-ALTER TABLE  `applicants_td` ADD FOREIGN KEY (  `reference_id` ) REFERENCES  `codeX_applicants_db`.`reference_td` (
-`reference_id`
-) ON DELETE RESTRICT ON UPDATE RESTRICT ;
+-- INSERT INTO applicants_td (applicant_id,coded_before,experiance,name,surname,email,cell_no,dob,city,educational_info,reference_id,applicant_ref_link) VALUES (1,"yes,quite a bit","wfewddWEEWD ERWFEWKHBKL BFWEQFWF YBGKJBYVKJ FFEWEFERERFFSE GREGE EW4F","Sphiwe","Ngcinga","oysiphula@gmail.com",0781268545,1994-02-01,"Cape Town","I'm currently having IT certificate",1,"Mentor");
 
-ALTER TABLE  `applicants_td` ADD FOREIGN KEY (  `mail_id` ) REFERENCES  `codeX_applicants_db`.`codecademy_mailAdrresses` (
-`mail_id`
-) ON DELETE RESTRICT ON UPDATE RESTRICT ;
+-- INSERT INTO reference_td (name,surname,email,cell_no) VALUES ("sphokazi","Peter","Sp1@gmail.com",0246563325);
 
--- Dumping data for table `category`
-
-INSERT INTO reference_td (name,surname,email,cell_no) VALUES ("sphokazi","Peter","Sp1@gmail,com",0246563325);
-INSERT INTO reference_td (name,surname,email,cell_no) VALUES ("sphokazi","Peter","Sp1@gmail,com",0246563325);
-INSERT INTO reference_td (name,surname,email,cell_no) VALUES ("sphokazi","Peter","Sp1@gmail,com",0246563325);
-
-INSERT INTO applicants_td (name,surname,email,cell_no,dob,city,educational_info,reference_id,applicant_ref_link) VALUES ("Sphiwe","Ngcinga","oysiphula@gmail.com",0781268545,1994-02-01,"Cape Town","I'm currently having IT certificate",1,"Mentor");
+-- INSERT INTO codecademy_mailAdrresses (email) VALUES ("Sphiwe@projectcodex.co");
